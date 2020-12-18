ec0f8a81a3f520271a91ac834a2af2283c34d0fc
AnanditaRian19 <rizkianandita6@gmail.com>
2020-12-18 11:38:43 +0700

    Update for My Intent App

--- 01-MyIntentApp/app/src/main/java/com/kotlin/myintentapp/MainActivity.kt
+++ 01-MyIntentApp/app/src/main/java/com/kotlin/myintentapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.kotlin.myintentapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btnMoveActivity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnMoveActivity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 }
