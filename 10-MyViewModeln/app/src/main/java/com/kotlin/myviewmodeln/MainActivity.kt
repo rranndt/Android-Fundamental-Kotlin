45cf2f634d68c8ddcad9a14a65711c9696cf93dd
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-08 00:07:33 +0700

    Update: MainActivity.kt

--- 10-MyViewModeln/app/src/main/java/com/kotlin/myviewmodeln/MainActivity.kt
+++ 10-MyViewModeln/app/src/main/java/com/kotlin/myviewmodeln/MainActivity.kt
@@ -2,10 +2,45 @@ package com.kotlin.myviewmodeln
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import com.kotlin.myviewmodeln.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setMyButtonEnable()
+
+        binding.myEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+        binding.myButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                binding.myEditText.text,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun setMyButtonEnable() {
+        val result = binding.myEditText.text
+        binding.myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
