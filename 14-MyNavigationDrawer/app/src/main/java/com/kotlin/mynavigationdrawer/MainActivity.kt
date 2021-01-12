f49e379820361d48c8109ae1a487d5df59681317
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-12 23:04:34 +0700

    Update fix

--- 14-MyNavigationDrawer/app/src/main/java/com/kotlin/mynavigationdrawer/MainActivity.kt
+++ 14-MyNavigationDrawer/app/src/main/java/com/kotlin/mynavigationdrawer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kotlin.mynavigationdrawer
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,36 +14,58 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.bumptech.glide.Glide
+import de.hdodenhof.circleimageview.CircleImageView
 
-class Main2Activity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
+    internal lateinit var profileCircleImageView: CircleImageView
+    internal var profileImageUrl =
+        "https://lh3.googleusercontent.com/-4qy2DfcXBoE/AAAAAAAAAAI/AAAAAAAABi4/rY-jrtntAi4/s640-il/photo.jpg"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action") {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Halo ini action dari snackbar",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .show()
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        profileCircleImageView = navView.getHeaderView(0).findViewById(R.id.imageView)
+        Glide.with(this)
+            .load(profileImageUrl)
+            .into(profileCircleImageView)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_cart, R.id.nav_subway
+            ),
+            drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main2, menu)
+        menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
---
+++ 14-MyNavigationDrawer/app/src/main/java/com/kotlin/mynavigationdrawer/ui/subway/SubwayActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.mynavigationdrawer.ui.subway
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.mynavigationdrawer.R
+
+class SubwayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
