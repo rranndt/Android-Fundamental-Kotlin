800cff3d00cb443b77825c2c80e2f687f46ae558
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-12 11:24:01 +0700

    Add: adapter ListHeroAdapter.kt

---
+++ 11-MyRecyclerView/app/src/main/java/com/kotlin/myrecyclerview/Adapter/ListHeroAdapter.kt
@@ -0,0 +1,48 @@
+package com.kotlin.myrecyclerview.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.kotlin.myrecyclerview.Model.Hero
+import com.kotlin.myrecyclerview.databinding.ItemRowHeroBinding
+
+/**
+ *@author Rizki Rian Anandita
+ * Create By rizki
+ */
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
+    RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListHeroAdapter.ListViewHolder {
+        val binding = ItemRowHeroBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListHeroAdapter.ListViewHolder, position: Int) {
+        holder.bind(listHero[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listHero.size
+    }
+
+    class ListViewHolder(private val binding: ItemRowHeroBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(hero: Hero) {
+            with(binding) {
+                Glide.with(itemView.context)
+                    .load(hero.photo)
+                    .apply(RequestOptions().override(55, 55))
+                    .into(ivPhoto)
+                tvItemName.text = hero.name
+                tvItemDesc.text = hero.description
+            }
+        }
+    }
+
+}
