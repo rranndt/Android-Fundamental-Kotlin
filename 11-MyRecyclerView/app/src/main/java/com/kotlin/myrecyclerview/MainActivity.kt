83594fded71b2813a701a2cb5859def36c990ff5
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-12 11:59:36 +0700

    Update: set mode in actionbar

--- 11-MyRecyclerView/app/src/main/java/com/kotlin/myrecyclerview/MainActivity.kt
+++ 11-MyRecyclerView/app/src/main/java/com/kotlin/myrecyclerview/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.kotlin.myrecyclerview.Adapter.CardViewHeroAdapter
 import com.kotlin.myrecyclerview.Adapter.GridHeroAdapter
 import com.kotlin.myrecyclerview.Adapter.ListHeroAdapter
 import com.kotlin.myrecyclerview.Model.Hero
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val list = ArrayList<Hero>()
 
+    private var title = "Mode List"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
         list.addAll(getListHero())
         showRecyclerList()
+        setActionTitle(title)
     }
 
     fun getListHero(): ArrayList<Hero> {
@@ -56,6 +60,12 @@ class MainActivity : AppCompatActivity() {
         binding.rvHeroes.adapter = gridHeroAdapter
     }
 
+    private fun showRecyclerCardView() {
+        binding.rvHeroes.layoutManager = LinearLayoutManager(this)
+        val cardViewHeroAdapter = CardViewHeroAdapter(list)
+        binding.rvHeroes.adapter = cardViewHeroAdapter
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
@@ -69,14 +79,22 @@ class MainActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when (selectedMode) {
             R.id.actionList -> {
+                title = "Mode List"
                 showRecyclerList()
             }
             R.id.actionGrid -> {
+                title = "Mode Grid"
                 showRecyclerGrid()
             }
             R.id.actionCardview -> {
-
+                title = "Mode CardView"
+                showRecyclerCardView()
             }
         }
+        setActionTitle(title)
+    }
+
+    private fun setActionTitle(title: String?) {
+        supportActionBar?.title = title
     }
 }
