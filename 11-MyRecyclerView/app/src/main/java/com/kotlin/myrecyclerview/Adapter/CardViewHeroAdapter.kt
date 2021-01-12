796bac948a30ec9b37229679367bbb70f056fa0d
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-12 12:34:38 +0700

    Update: OnClick

--- 11-MyRecyclerView/app/src/main/java/com/kotlin/myrecyclerview/Adapter/CardViewHeroAdapter.kt
+++ 11-MyRecyclerView/app/src/main/java/com/kotlin/myrecyclerview/Adapter/CardViewHeroAdapter.kt
@@ -2,6 +2,7 @@ package com.kotlin.myrecyclerview.Adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -39,8 +40,25 @@ class CardViewHeroAdapter(private val listHero: ArrayList<Hero>) :
                     .into(ivPhoto)
                 tvItemName.text = hero.name
                 tvItemDescription.text = hero.description
+
+                btnSetFavorite.setOnClickListener {
+                    Toast.makeText(itemView.context, "Favorite ${hero.name}", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                btnSetShare.setOnClickListener {
+                    Toast.makeText(itemView.context, "Share ${hero.name}", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                itemView.setOnClickListener {
+                    Toast.makeText(
+                        itemView.context,
+                        "Kamu memilih ${hero.name}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
     }
-
 }
