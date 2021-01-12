5fcc174af0066a68a04690d50f3c40a6fa9a1fb0
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-12 12:31:39 +0700

    Add: OnClick List

---
+++ 11-MyRecyclerView/app/src/main/java/com/kotlin/myrecyclerview/ItemClickCallback.kt
@@ -0,0 +1,13 @@
+package com.kotlin.myrecyclerview
+
+import com.kotlin.myrecyclerview.Model.Hero
+
+/**
+ *@author Rizki Rian Anandita
+ * Create By rizki
+ */
+interface ItemClickCallback {
+
+    fun onItemClicked(data: Hero)
+
+}
