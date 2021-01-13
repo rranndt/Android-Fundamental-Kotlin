f0f7706e8e88b68518393be54ac3a235d3189a2a
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-14 00:15:18 +0700

    Update: Fix All

--- 17-MyLocalization/app/src/main/java/com/kotlin/mylocalization/MainActivity.kt
+++ 17-MyLocalization/app/src/main/java/com/kotlin/mylocalization/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.kotlin.mylocalization
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
+import com.kotlin.mylocalization.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pokeCount = 3
+        val hello = resources.getString(R.string.hello_world, "Narenda Wicaksono", pokeCount, "Yoza Aprilio")
+
+        binding.tvHello.text = hello
+
+        val songCount = 5
+        val pluralText = resources.getQuantityString(R.plurals.numberOfSongsAvailable, songCount, songCount)
+        binding.tvPlural.text = pluralText
+
+        binding.tvXliff.text = resources.getString(R.string.app_homeurl)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_change_settings) {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
