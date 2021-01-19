f658fd20b76ee08227421ef1b58ddaab48001617
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-19 22:06:15 +0700

    Update: Fix Thread with coroutine

--- 19-MyBackgroundThread/app/src/main/java/com/kotlin/mybackgroundthread/MainActivity.kt
+++ 19-MyBackgroundThread/app/src/main/java/com/kotlin/mybackgroundthread/MainActivity.kt
@@ -2,10 +2,57 @@ package com.kotlin.mybackgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.lifecycleScope
+import com.kotlin.mybackgroundthread.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+        // With coroutine
+        binding.btnStart.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Default) {
+                // Simulate Process in background thread
+                try {
+                    for (i in 0..10) {
+                        delay(500)
+                        val percentage = i * 10
+                        withContext(Dispatchers.Main) {
+                            // Update ui in main thread
+                            if (percentage == 100) {
+                                binding.tvStatus.setText(R.string.task_complete)
+                            } else {
+                                binding.tvStatus.text =
+                                    String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
